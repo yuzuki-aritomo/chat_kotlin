6365b4a4181ec83a6d8f34d3e8f559e651fa0db3
yuzuki-aritomo <yuzuki.aritomo@gmail.com>
2020-08-29 00:41:42 +0900

    auth機能の追加

--- app/src/main/java/com/example/chatkotlin/MainActivity.kt
+++ app/src/main/java/com/example/chatkotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,18 +15,29 @@ class MainActivity : AppCompatActivity() {
 
         //register
         start_btn_register.setOnClickListener{
-            val email = email_register.text.toString()
-            val password = password_register.text.toString()
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if(!it.isSuccessful) return@addOnCompleteListener
-                    Log.d("Main","Successfully created user with uid:")
-                }
+            performRegister()
         }
         //login
         btn_login.setOnClickListener{
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+    //register
+    private fun performRegister(){
+        val email = email_register.text.toString()
+        val password = password_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this,"Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("Main","btn pushed ${email}")
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+                Log.d("Main","Successfully created user with uid:")
+            }
     }
 }
