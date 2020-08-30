41f410fad75d6193ae5d604a380ed7954efccfca
yuzuki-aritomo <yuzuki.aritomo@gmail.com>
2020-08-30 23:25:03 +0900

    commit 8/30

---
+++ app/src/main/java/com/example/chatkotlin/LatestMassageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chatkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LatestMassage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_massage)
+    }
+}
