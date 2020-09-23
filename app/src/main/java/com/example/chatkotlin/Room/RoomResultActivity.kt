7ac1bda1cb72f8659771801ed8beb3aa3b42bc46
yuzuki aritomo <59410309+yuzuki-aritomo@users.noreply.github.com>
2020-09-23 21:33:18 +0900

    commit before user_id

--- app/src/main/java/com/example/chatkotlin/Room/RoomResultActivity.kt
+++ app/src/main/java/com/example/chatkotlin/Room/RoomResultActivity.kt
@@ -16,19 +16,36 @@ class RoomResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_result)
         //room_idの設定
-        val room_id = intent.getStringExtra("room_id").toString()//room_id: room_1
-        val user_id = intent.getStringExtra("user_id").toString() //room_id: room_1
+//        val room_id = intent.getStringExtra("room_id").toString()//room_id: room_1
+//        val user_id = intent.getStringExtra("user_id").toString() //room_id: room_1
+
+        val room_id = "room_1"
+        val user_id = "-MHungwKH0BZGzbIFxB0"
 
         val ref = FirebaseDatabase.getInstance().getReference("Room/$room_id/user").orderByChild("score")
         ref.addListenerForSingleValueEvent(object: ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
-                Log.d("finish",p0.toString())
+                val layout_name = findViewById<LinearLayout>(R.id.line_layout_all)
+                var i = 0
+                p0.children.forEach {
+                    val line_ranking: LinearLayout = layout_name.getChildAt(i) as LinearLayout
+                    val textView_score: TextView = line_ranking.getChildAt(1) as TextView
+                    val textView_name: TextView = line_ranking.getChildAt(2) as TextView
+
+                    textView_score.text = it.child("score").value.toString()
+                    textView_name.text = it.child("user_name").value.toString()
+
+                    Log.d("nnnn",it.child("score").value.toString())
+                    Log.d("nnnn",it.toString())
+                    Log.d("nnnn",it.child("user_name").value.toString())
+                    i++
+                }
             }
             override fun onCancelled(p0: DatabaseError) {
             }
         })
-        val ref_finish = FirebaseDatabase.getInstance().getReference("Room/$room_id/finish")
-        ref_finish.setValue(user_id)
+//        val ref_finish = FirebaseDatabase.getInstance().getReference("Room/$room_id/finish")
+//        ref_finish.setValue(user_id)
 
     }
 }
